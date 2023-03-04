d44adb92b3161dd376a11b745b84935992a6b2c7
Benoit Essiambre <b.essiambre@gmail.com>
2023-03-04 14:12:11 -0400

    more

--- main.go
+++ main.go
@@ -263,8 +263,6 @@ func snakeToGoCamel(name string) string {
 }
 
 func sqlTypeToOa3SpecType(in *pb.Column) string {
-	fmt.Fprintln(os.Stderr, "Name", in.Type.Name)
-	fmt.Fprintln(os.Stderr, "Schema", in.Type.Schema)
 	typeStr := "type: object"
 
 	if in.Type.Schema != "pg_catalog" && in.Type.Schema != "" && in.Type.Name != "citext" {
@@ -299,12 +297,10 @@ func sqlTypeToOa3SpecType(in *pb.Column) string {
 		typeStr += ", nullable: true"
 	}
 
-	fmt.Fprintln(os.Stderr, "typeStr", typeStr)
-
 	if in.IsArray {
 		return fmt.Sprintf("{ type: array, items: { %s } }", typeStr)
 	} else {
-		return fmt.Sprintf("{ %s }", typeStr)
+		return fmt.Sprintf("{ %s } # %s %s", typeStr, in.Type.Schema, in.Type.Name)
 	}
 }
 
