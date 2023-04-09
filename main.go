21db06878727948ef1082c104856cfed3b6a7854
Benoit Essiambre <b.essiambre@gmail.com>
2023-04-09 15:32:23 -0300

    test types

--- main.go
+++ main.go
@@ -297,7 +297,7 @@ func sqlTypeToOa3SpecType(in *pb.Column) string {
 			typeStr = "type: number, format: float"
 		case "float8", "pg_catalog.float8":
 			typeStr = "type: number, format: double"
-		case "text", "varchar", "pg_catalog.text", "pg_catalog.varchar":
+		case "text", "varchar", "char", "pg_catalog.text", "pg_catalog.varchar", "pg_catalog.char":
 			typeStr = "type: string"
 		case "date", "pg_catalog.date":
 			typeStr = "type: string, format: date"
@@ -354,7 +354,7 @@ func sqlToHandlerParam(in *pb.Column) string {
 			typeStr = "float64"
 		case "float8", "pg_catalog.float8":
 			typeStr = "float64"
-		case "text", "varchar", "pg_catalog.text", "pg_catalog.varchar":
+		case "text", "varchar", "char", "pg_catalog.text", "pg_catalog.varchar", "pg_catalog.char":
 			typeStr = "string"
 		case "date", "pg_catalog.date":
 			typeStr = "chrono.Date"
